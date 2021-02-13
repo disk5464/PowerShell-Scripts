9ae6f62db2cf1571bff8e425e8bad923a3c39971
disk5464 <36386147+disk5464@users.noreply.github.com>
2021-02-13 15:46:48 -0500

    Add files via upload

---
+++ Display Path Contents.ps1
@@ -0,0 +1,27 @@
+﻿#This asks the user for a folder path and then displays all the files within along with the total size 
+###############################################################
+#Ask for folder path
+$Usrimp = Read-Host -Prompt "Enter file path"
+
+#Find any ` and replace them with '
+if($Usrimp -contains "`"")
+{
+    $Usrimp -replace"`","""
+
+}
+
+#Get the items
+$folder = Get-ChildItem $Usrimp
+
+#This gets size of the folder and rounds it to two digits
+$SizeToBeFormated = (Get-ChildItem $Usrimp -recurse  -ErrorAction SilentlyContinue | Measure-Object -property length -sum).sum / 1GB
+$TotalSize  = [math]::Round($SizeToBeFormated,2)
+
+#Write out each item 
+Foreach ($item in $folder)
+{
+            Write-Host $item
+}
+
+write-Host  $TotalSize -ForegroundColor  yellow
+Read-Host -Prompt "press any key to close"
