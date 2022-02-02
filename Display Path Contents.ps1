86dba18a2ed9c26e8a96cb264df53e654e9ad07c
disk5464 <36386147+disk5464@users.noreply.github.com>
2022-02-01 20:39:03 -0500

    Add files via upload

---
+++ Display Path Contents.ps1
@@ -0,0 +1,51 @@
+﻿#This asks the user for a folder path and then displays all the files within along with the total size 
+###############################################################
+#Ask for folder path
+$Usrimp = Read-Host -Prompt "Enter file path"
+
+#Find any ` and replace them with '
+if($Usrimp -contains "`"")
+{
+    $Usrimp -replace"`","""
+
+}
+
+#Get the items
+$folder = Get-ChildItem $Usrimp
+
+#This gets size of the folder and rounds it to two digits
+$SizeToBeFormated = (Get-ChildItem $Usrimp -recurse  -ErrorAction SilentlyContinue | Measure-Object -property length -sum).sum / 1GB
+$TotalSize  = [math]::Round($SizeToBeFormated,2)
+
+#Create an array so we can add the items that we will send to the clipboard
+$ItemArry = @()
+
+#Write out each item 
+Foreach ($item in $folder)
+{
+           if($item -like "*.torrent*")
+           {
+                #Take item and remove .torrent from the end
+                $trimedItem = $item.ToString().TrimEnd(".torrent")
+                Write-Host $trimedItem
+                
+                #add the item to the array
+                $ItemArry += $trimedItem
+           }
+           else
+           {
+                Write-Host $item
+
+                #Add the item to the array
+                $ItemArry += $item
+           }
+}
+
+#set the clipboard to the array
+Set-Clipboard $ItemArry
+Write-Host "Sent the content to the clipboard" -ForegroundColor Yellow
+
+#Display the size of the folder and a read-host so PS dosen't auto close
+write-Host  $TotalSize "GB" -ForegroundColor  yellow
+Read-Host -Prompt "press any key to close"
+
